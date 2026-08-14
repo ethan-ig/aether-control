4690ecb631a2f8a40953b6c24534d7829c11a6f4
vaticangiftshop <doctorwhofan545@gmail.com>
2026-08-14 17:09:01 -0400

    Add kiosk launcher

---
+++ kiosk.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+URL="${AETHER_CONTROL_URL:-http://127.0.0.1:5000}"
+exec chromium \
+  --kiosk \
+  --noerrdialogs \
+  --disable-infobars \
+  --disable-session-crashed-bubble \
+  --disable-translate \
+  --overscroll-history-navigation=0 \
+  --disable-pinch \
+  "$URL"
